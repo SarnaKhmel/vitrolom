f9b27d3466c5a39fb7cc0852aa60ce517dbcf9e6
sarnakhmel <OlSa@olsa.local>
2023-02-16 16:46:00 +0200

    fin header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,13 @@
 import LogoBox from "./LogoBox/LogoBox";
 import HeaderBar from "./HeaderBar/HeaderBar";
+import { Box } from "./Header.styles";
 
 const Header = () => {
   return (
-    <>
+    <Box>
       <LogoBox />
       <HeaderBar />
-    </>
+    </Box>
   );
 };
 
--- src/components/Header/HeaderBar/Bar.styles.js
+++ src/components/Header/HeaderBar/Bar.styles.js
@@ -11,6 +11,7 @@ export const BarBtn = styled.a`
   color: #164e5f;
   &:hover,
   &:focus {
+    cursor: pointer;
     box-shadow: inset 0px 2px 1px -1px rgb(0 0 0 / 20%),
       0px 1px 1px 0px rgb(0 0 0 / 14%), 0px 1px 3px 0px rgb(0 0 0 / 12%);
   }
@@ -24,6 +25,7 @@ export const BarBtnHelp = styled.a`
   color: #fff;
   &:hover,
   &:focus {
+    cursor: pointer;
     box-shadow: inset 0px 2px 1px -1px rgb(0 0 0 / 20%),
       0px 1px 1px 0px rgb(0 0 0 / 14%), 0px 1px 3px 0px rgb(0 0 0 / 12%);
   }
