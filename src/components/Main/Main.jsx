532e4d486cc57fbf356fc89494c23f80439cf32b
sarnakhmel <OlSa@olsa.local>
2023-06-28 17:24:22 +0300

    layout

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,7 @@
-import Header from "../Header/Header";
 import MainBlock from "./MainBlock/MainBlock";
 const Main = () => {
   return (
     <>
-      <Header />
       <MainBlock />
     </>
   );
